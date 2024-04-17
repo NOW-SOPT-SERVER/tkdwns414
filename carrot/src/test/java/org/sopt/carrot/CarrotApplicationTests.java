7ce67c5661788fdd93d30b25b387aecc49a833f1
tkdwns414 <sangjune2000@naver.com>
2024-04-17 14:20:04 +0900

    [init] #5 Init project

---
+++ carrot/src/test/java/org/sopt/carrot/CarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
